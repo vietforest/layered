5a56e32e543dc7cfa034a1c0ca5f36bba7988299
vietforest <nguyenhuynhanh1954@gmail.com>
2026-09-23 11:36:22 +0200

    s

--- be/app/app.py
+++ be/app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
 DB_NAME = os.environ.get("DB_NAME", "esempio")
 
 
